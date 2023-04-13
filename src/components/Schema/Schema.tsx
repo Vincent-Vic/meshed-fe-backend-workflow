b2c67f58494eb797fe4c7c5ad8ccb157bb1f83f9
Vincent Vic <vic.coding@gmail.com>
2023-04-13 17:40:37 +0800

    feat: 表单抽象组件化

--- src/components/Schema/Schema.tsx
+++ src/components/Schema/Schema.tsx
@@ -1,8 +1,32 @@
 import React from "react";
 import { createForm } from '@formily/core'
 import { createSchemaField } from '@formily/react'
-import { Form, FormItem, Input, Password } from '@formily/antd'
+import {
+  Form,
+  FormItem,
+  Input,
+  Password,
+  Select,
+  TreeSelect,
+  Cascader,
+  Radio,
+  Checkbox,
+  NumberPicker,
+  Transfer,
+  DatePicker,
+  TimePicker,
+  Upload,
+  Switch,
+  ArrayCards,
+  ArrayTable,
+  Space,
+  FormTab,
+  FormCollapse,
+  FormLayout,
+  FormGrid,
+} from '@formily/antd'
 import * as ICONS from '@ant-design/icons'
+import {Card, ObjectContainer, Rate, Slider} from "@designable/formily-antd";
 
 
 export const Schema = createSchemaField({
@@ -11,6 +35,28 @@ export const Schema = createSchemaField({
     FormItem,
     Input,
     Password,
+    Select,
+    TreeSelect,
+    Cascader,
+    Radio,
+    Checkbox,
+    Slider,
+    Rate,
+    NumberPicker,
+    Transfer,
+    DatePicker,
+    TimePicker,
+    Upload,
+    Switch,
+    Card,
+    ArrayCards,
+    ObjectContainer,
+    ArrayTable,
+    Space,
+    FormTab,
+    FormCollapse,
+    FormLayout,
+    FormGrid,
   },
   scope: {
     icon(name: string | number) {
