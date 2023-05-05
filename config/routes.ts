e8707b82a748660c2eec0cb10a1b312eff716b77
Vincent Vic <vic.coding@gmail.com>
2023-05-05 15:18:52 +0800

    feat: 权限控制

--- config/routes.ts
+++ config/routes.ts
@@ -7,16 +7,31 @@ export default [
   {
     path: '/flow/designable/:type/:flowId',
     layout: false,
+    access: 'canAdmin',
     name: '工作流设计器',
     icon: 'CodeOutlined',
     component: './FlowDesignable'
   },
-  {path: '/form/designable/:formId', layout: false, name: '表单设计器', icon: 'CodeOutlined', component: './FormDesignable'},
-  {path: '/task/', name: '任务中心', icon: 'ReconciliationOutlined', component: './Task'},
-  {path: '/task/details/:type/:instanceId/:taskId', name: '任务详情', hideInMenu: true, icon: 'ReconciliationOutlined', component: './Task/Details'},
-  {path: '/definition/', name: '流程定义', icon: 'NodeIndexOutlined', component: './Definition'},
-  {path: '/form/', name: '动态表单', icon: 'CodeOutlined', component: './Form'},
+  {
+    path: '/form/designable/:formId',
+    access: 'canAdmin',
+    layout: false,
+    name: '表单设计器',
+    icon: 'CodeOutlined',
+    component: './FormDesignable'
+  },
+  {path: '/task/', name: '任务中心', access: 'canTask', icon: 'ReconciliationOutlined', component: './Task'},
+  {
+    path: '/task/details/:type/:instanceId/:taskId',
+    name: '任务详情',
+    access: 'canTask',
+    hideInMenu: true,
+    icon: 'ReconciliationOutlined',
+    component: './Task/Details'
+  },
+  {path: '/definition/', name: '流程定义', access: 'canAdmin', icon: 'NodeIndexOutlined', component: './Definition'},
+  {path: '/form/', name: '动态表单', access: 'canAdmin', icon: 'CodeOutlined', component: './Form'},
   {path: '/', redirect: '/task'},
-  { path: '/error/:code', layout: false, component: './error' },
-  { path: '*', redirect: '/error/404' },
+  {path: '/error/:code', layout: false, component: './error'},
+  {path: '*', redirect: '/error/404'},
 ];
--- src/access.ts
+++ src/access.ts
@@ -1,9 +1,12 @@
+import _ from "lodash";
+
 /**
  * @see https://umijs.org/zh-CN/plugins/plugin-access
  * */
 export default function access(initialState: { currentUser?: API.CurrentUser } | undefined) {
   const { currentUser } = initialState ?? {};
   return {
-    canAdmin: currentUser && currentUser.access === 'admin',
+    canAdmin: currentUser && currentUser.grantedRole && currentUser.grantedRole?.length > 0 && _.intersection(currentUser.grantedRole, ['FLOW:ADMIN']).length > 0 ,
+    canTask: currentUser && currentUser.grantedRole && currentUser.grantedRole?.length > 0 && _.intersection(currentUser.grantedRole, ['FLOW:ADMIN','FLOW:TASK']).length > 0 ,
   };
 }
